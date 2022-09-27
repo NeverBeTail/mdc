9e0e72b11f958571326d49b08ef842d9229d3b61
LEE.K.M <rudals5569@gmail.com>
2022-09-27 13:51:16 +0900

    first commit

---
+++ src/test/java/com/company/mdc/mdc/MdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.mdc.mdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
